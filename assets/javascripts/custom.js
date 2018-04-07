a18e8bebda77ad4b549c6a50300e4512f3a32c53
c-Cyril-l <cyrildouglas@protonmail.com>
2018-04-07 05:46:21 -0700

    Added background to header, made some improvements...

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -8,6 +8,34 @@ const tr = document.getElementsByTagName('tr');
     }
 })();
 
+(function() {
+    for (i = 0; i < tr.length; i++) {
+        let td = tr[i].firstElementChild;
+        td.addEventListener('mouseover', slangHover);
+        td.addEventListener('click', slangClick);
+        td.addEventListener('mouseleave', slangLeave)
+    }
+})();
+
+function slangHover(e) {
+    e.target.style.cursor = 'pointer';
+    e.target.style.color = '#7b1fa2';
+    e.target.style.fontWeight = "bold";
+
+}
+
+function slangClick(e) {
+    let prefixSearch = 'https://www.google.iq/search?q=';
+    let stringToSearch = prefixSearch + e.target.textContent.toUpperCase() + ' ' + 'abbreviation';
+    window.open(stringToSearch, '_blank');
+}
+
+function slangLeave(e) {
+    e.target.style.color = '#000';
+    e.target.style.fontWeight = "normal";
+}
+
+
 abbreviationsInput.addEventListener('keyup', filterAbbreviations);
 
 function filterAbbreviations(e) {
