1c2036ced76f5fb03b95c3dbf720be9d0df6304b
c-Cyril-l <cyrildouglas@protonmail.com>
2018-04-06 18:28:41 -0700

    Initial Release

---
+++ assets/javascripts/custom.js
@@ -0,0 +1,20 @@
+const abbreviationsInput = document.querySelector('#filter');
+const tr = document.getElementsByTagName('tr');
+
+
+abbreviationsInput.addEventListener('keyup', filterAbbreviations);
+
+function filterAbbreviations(e) {
+    let text = abbreviationsInput.value.toLowerCase();
+
+    for (i = 0; i < tr.length; i++) {
+        let td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.textContent.toLowerCase().indexOf(text) != -1) {
+                tr[i].style.display = '';
+            } else {
+                tr[i].style.display = 'none';
+            }
+        }
+    }
+}
