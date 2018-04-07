7c29980da5bbda6d37a67c20da99c0bcca52aa94
c-Cyril-l <cyrildouglas@protonmail.com>
2018-04-07 05:59:23 -0700

    Bugs fixed, clean up code

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -1,46 +1,44 @@
 const abbreviationsInput = document.querySelector('#filter');
 const tr = document.getElementsByTagName('tr');
+let target;
+
 
 (function() {
     for (i = 0; i < tr.length; i++) {
         let td = tr[i].firstElementChild;
         td.style.textTransform = "uppercase";
-    }
-})();
-
-(function() {
-    for (i = 0; i < tr.length; i++) {
-        let td = tr[i].firstElementChild;
-        td.addEventListener('mouseover', slangHover);
-        td.addEventListener('click', slangClick);
-        td.addEventListener('mouseleave', slangLeave)
+        if (td.textContent.toUpperCase() != 'ABBREVIATIONS') {
+            td.addEventListener('mouseover', slangHover);
+            td.addEventListener('click', slangClick);
+            td.addEventListener('mouseleave', slangLeave);
+        }
     }
 })();
 
 function slangHover(e) {
-    e.target.style.cursor = 'pointer';
-    e.target.style.color = '#7b1fa2';
-    e.target.style.fontWeight = "bold";
-
+    target = e.target;
+    target.style.cursor = 'pointer';
+    target.style.color = '#7b1fa2';
+    target.style.fontWeight = "bold";
 }
 
 function slangClick(e) {
+    target = e.target;
     let prefixSearch = 'https://www.google.iq/search?q=';
-    let stringToSearch = prefixSearch + e.target.textContent.toUpperCase() + ' ' + 'abbreviation';
+    let stringToSearch = prefixSearch + target.textContent.toUpperCase() + ' ' + 'abbreviation';
     window.open(stringToSearch, '_blank');
 }
 
 function slangLeave(e) {
-    e.target.style.color = '#000';
-    e.target.style.fontWeight = "normal";
+    target = e.target;
+    target.style.color = '#000';
+    target.style.fontWeight = "normal";
 }
-
 
 abbreviationsInput.addEventListener('keyup', filterAbbreviations);
 
 function filterAbbreviations(e) {
     let text = abbreviationsInput.value.toLowerCase();
-
     for (i = 0; i < tr.length; i++) {
         let td = tr[i].getElementsByTagName("td")[0];
         if (td) {
